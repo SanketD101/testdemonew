8fa0509df57a1b4cdeb83edab232002de49eb1e2
Sanket Dhagdi <67003509+SanketD101@users.noreply.github.com>
2023-10-17 22:53:44 +0530

    first commit

---
+++ src/main/java/com/testdemonew/TestdemonewApplication.java
@@ -0,0 +1,13 @@
+package com.testdemonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdemonewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdemonewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testdemonew/TestdemonewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdemonew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdemonewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
